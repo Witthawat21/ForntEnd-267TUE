77be98a3046a62142f59ad9b8b013498be445b72
Witthawat21 <fernovenlove@gmail.com>
2025-02-18 09:26:31 +0700

    Test2

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const bodyParser = require("body-parser");
+app.use(express.static("public"));
+
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
